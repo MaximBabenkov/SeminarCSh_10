41adcf0a168b53772b028ecdf1fecb65b8e63bbf
MaximBabenkov <lostbass@mail.ru>
2022-06-19 11:52:24 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа при помощи рекурсии
+int SumDigOfNumb(int number)
+{
+    if (number==0) return 0;
+    else return number%10+SumDigOfNumb(number/10);
+}
+
+Console.Clear();
+Console.Write("Enter your number: ");
+int a=int.Parse(Console.ReadLine()??"0");
+Console.WriteLine($"{SumDigOfNumb(a)}");
